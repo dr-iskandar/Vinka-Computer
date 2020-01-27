3f0e13d85e73934d4813d2a1c1ed06e6923648d9
dr-iskandar <drachmat.iskandar@gmail.com>
2020-01-27 16:37:43 +0700

    Initial commit

---
+++ Assets/Script/loading.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class loading : MonoBehaviour {
+
+public Transform LoadingReload;
+
+[SerializeField] private float currentAmount;
+[SerializeField] private float speed;
+
+void Update () {
+	if (currentAmount < 100) {
+		currentAmount += speed * Time.deltaTime;
+		Debug.Log ((int)currentAmount);
+	} else {
+			Application.LoadLevel ("MainMenu");
+	}
+
+	LoadingReload.GetComponent<Image> ().fillAmount = currentAmount / 100;
+ }
+}
